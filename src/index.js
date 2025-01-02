3c5d1791ae9386774e8958972ba4b019fb4480bc
9567828 <kimssi9567@gmail.com>
2025-01-02 23:14:00 +0900

    빌드작업

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <App />
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 
   // 감싸져 있으면 render를 두번함
   // <React.StrictMode>
